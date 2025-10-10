31f0df0a38c822921aabd5780a6c91d4ac1922ba
FloAvis <152602773+FloAvis@users.noreply.github.com>
2025-10-10 19:59:18 +0200

    Chaning origin of tabICL to be modified version

--- MultiLabelPFN/src/tabicl/train/run.py
+++ MultiLabelPFN/src/tabicl/train/run.py
@@ -21,11 +21,11 @@ from torch.distributed import init_process_group, destroy_process_group
 from tqdm import tqdm
 import wandb
 
-from tabicl import TabICL
-from tabicl.prior.dataset import PriorDataset
-from tabicl.prior.genload import LoadPriorDataset
-from tabicl.train.optim import get_scheduler
-from tabicl.train.train_config import build_parser
+from MultiLabelPFN.src.tabicl import TabICL
+from MultiLabelPFN.src.tabicl.prior.dataset import PriorDataset
+from MultiLabelPFN.src.tabicl.prior.genload import LoadPriorDataset
+from MultiLabelPFN.src.tabicl.train.optim import get_scheduler
+from MultiLabelPFN.src.tabicl.train.train_config import build_parser
 
 warnings.filterwarnings(
     "ignore", message=".*The PyTorch API of nested tensors is in prototype stage.*", category=UserWarning
