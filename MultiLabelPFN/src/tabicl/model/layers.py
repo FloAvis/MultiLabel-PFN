efbd816dc60436a92752564aa762003d1e398fe7
FloAvis <152602773+FloAvis@users.noreply.github.com>
2025-10-15 15:49:58 +0200

    Bugfixing sklearn prediciton 7

--- MultiLabelPFN/src/tabicl/model/layers.py
+++ MultiLabelPFN/src/tabicl/model/layers.py
@@ -83,7 +83,7 @@ class LabelLinear(nn.Linear):
         """
 
         #one_hot = F.one_hot(src.long(), self.num_classes).to(src.dtype)
-        print(src)
+        print(src.shape)
         # apply linear projection
         return F.linear(src, self.weight, self.bias)
 
