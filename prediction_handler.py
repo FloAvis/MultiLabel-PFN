0556ba3eb3e67471b65cf680c5c3606972bb43fd
FloAvis <152602773+FloAvis@users.noreply.github.com>
2025-10-20 13:25:06 +0200

    fixing MuLaTabICL cv prediction 7

--- prediction_handler.py
+++ prediction_handler.py
@@ -211,13 +211,9 @@ def cv_predict(model, X, Y, cv, mode="single", method="predict"):
                 y_pred_tmp = model.predict_proba(X_arr[test_idx])
 
             if method == "predict_proba" and len(y_pred_tmp.shape) == 2:
-                if mode == "single":
-                    y_pred = np.zeros((X.shape[0], Y.shape[1]))
-                    y_true = np.zeros((X.shape[0], Y.shape[1]))  # (n_samples, n_labels, n_classes)
-                elif mode == "ensemble":
-                    y_pred = np.zeros((model.n_jobs, X.shape[0], Y.shape[1]))  # (n_samples, n_labels, n_classes)
-                    y_true = np.zeros((model.n_jobs, X.shape[0], Y.shape[1]))  # (n_samples, n_labels, n_classes)
-
+                y_pred_tmp_tmp = np.zeros_like(y_pred)
+                y_pred_tmp_tmp[:,:,0] = y_pred_tmp
+                y_pred_tmp = y_pred_tmp_tmp
                 predict_proba_classSelect = False
 
 
