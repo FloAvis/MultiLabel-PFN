d87b79496051b5c35b31bfd0a785994b39a51ea5
FloAvis <152602773+FloAvis@users.noreply.github.com>
2025-10-28 16:16:01 +0100

    Trying running tabpfn 005

--- MultiLabelTabPFN/src/tabpfn/__init__.py
+++ MultiLabelTabPFN/src/tabpfn/__init__.py
@@ -1,9 +1,9 @@
 from importlib.metadata import version
 
-from tabpfn.classifier import TabPFNClassifier
-from tabpfn.misc.debug_versions import display_debug_info
-from tabpfn.model_loading import load_fitted_tabpfn_model, save_fitted_tabpfn_model
-from tabpfn.regressor import TabPFNRegressor
+from MultiLabelTabPFN.src.tabpfn.classifier import TabPFNClassifier
+from MultiLabelTabPFN.src.tabpfn.misc.debug_versions import display_debug_info
+from MultiLabelTabPFN.src.tabpfn.model_loading import load_fitted_tabpfn_model, save_fitted_tabpfn_model
+from MultiLabelTabPFN.src.tabpfn.regressor import TabPFNRegressor
 
 try:
     __version__ = version(__name__)
--- MultiLabelTabPFN/src/tabpfn/preprocessors/__init__.py
+++ MultiLabelTabPFN/src/tabpfn/preprocessors/__init__.py
@@ -1,33 +1,33 @@
-from tabpfn.preprocessors.adaptive_quantile_transformer import (
+from MultiLabelTabPFN.src.tabpfn.preprocessors.adaptive_quantile_transformer import (
     AdaptiveQuantileTransformer,
 )
-from tabpfn.preprocessors.add_fingerprint_features_step import (
+from MultiLabelTabPFN.src.tabpfn.preprocessors.add_fingerprint_features_step import (
     AddFingerprintFeaturesStep,
 )
-from tabpfn.preprocessors.differentiable_z_norm_step import DifferentiableZNormStep
-from tabpfn.preprocessors.encode_categorical_features_step import (
+from MultiLabelTabPFN.src.tabpfn.preprocessors.differentiable_z_norm_step import DifferentiableZNormStep
+from MultiLabelTabPFN.src.tabpfn.preprocessors.encode_categorical_features_step import (
     EncodeCategoricalFeaturesStep,
 )
-from tabpfn.preprocessors.kdi_transformer import (
+from MultiLabelTabPFN.src.tabpfn.preprocessors.kdi_transformer import (
     KDITransformerWithNaN,
     get_all_kdi_transformers,
 )
-from tabpfn.preprocessors.nan_handling_polynomial_features_step import (
+from MultiLabelTabPFN.src.tabpfn.preprocessors.nan_handling_polynomial_features_step import (
     NanHandlingPolynomialFeaturesStep,
 )
-from tabpfn.preprocessors.preprocessing_helpers import (
+from MultiLabelTabPFN.src.tabpfn.preprocessors.preprocessing_helpers import (
     FeaturePreprocessingTransformerStep,
     SequentialFeatureTransformer,
 )
-from tabpfn.preprocessors.remove_constant_features_step import (
+from MultiLabelTabPFN.src.tabpfn.preprocessors.remove_constant_features_step import (
     RemoveConstantFeaturesStep,
 )
-from tabpfn.preprocessors.reshape_feature_distribution_step import (
+from MultiLabelTabPFN.src.tabpfn.preprocessors.reshape_feature_distribution_step import (
     ReshapeFeatureDistributionsStep,
     get_all_reshape_feature_distribution_preprocessors,
 )
-from tabpfn.preprocessors.safe_power_transformer import SafePowerTransformer
-from tabpfn.preprocessors.shuffle_features_step import ShuffleFeaturesStep
+from MultiLabelTabPFN.src.tabpfn.preprocessors.safe_power_transformer import SafePowerTransformer
+from MultiLabelTabPFN.src.tabpfn.preprocessors.shuffle_features_step import ShuffleFeaturesStep
 
 __all__ = [
     "AdaptiveQuantileTransformer",
