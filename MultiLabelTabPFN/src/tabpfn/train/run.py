f5fe762946d2ee12bf44b3ee837d5eaa6c4cc3fe
FloAvis <152602773+FloAvis@users.noreply.github.com>
2025-10-28 16:13:09 +0100

    Trying running tabpfn 004

--- MultiLabelTabPFN/src/tabpfn/train/run.py
+++ MultiLabelTabPFN/src/tabpfn/train/run.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
-import os
+import sys, os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../..")))
+
+
 import timeit
 import warnings
 import functools
