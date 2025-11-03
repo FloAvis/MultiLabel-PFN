ff14438b1f7b9e3dc1a3134845485114258edc4b
FloAvis <152602773+FloAvis@users.noreply.github.com>
2025-11-03 21:11:02 +0100

    predictiong tabpfn attemtp 6

--- MultiLabelTabPFN/src/tabpfn/architectures/base/__init__.py
+++ MultiLabelTabPFN/src/tabpfn/architectures/base/__init__.py
@@ -47,7 +47,7 @@ def parse_config(config: dict[str, Any]) -> tuple[ArchitectureConfig, dict[str, 
     """
     #upgraded_dict = ModelConfig.upgrade_config(config)
     print(type(config))
-    parsed_config = ModelConfig(**config)
+    parsed_config = ModelConfig(config)
     return parsed_config, parsed_config.get_unused_config(config)
 
 
