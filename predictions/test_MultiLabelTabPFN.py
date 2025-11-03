bfdf1a8f35927194e7c32f694a74462e5520dfba
FloAvis <152602773+FloAvis@users.noreply.github.com>
2025-11-03 15:31:54 +0100

    Running new models debug 5

--- predictions/test_MultiLabelTabPFN.py
+++ predictions/test_MultiLabelTabPFN.py
@@ -27,7 +27,7 @@ def main():
     #files = [r"../data/PI_DataSet.txt", r"../data/INI_DataSet.txt", r"../data/NRTI_DataSet.txt",
     #         r"../data/NNRTI_DataSet.txt"]
 
-    files = [r"../data/INI_DataSet.txt", r"../data/NRTI_DataSet.txt",
+    files = [r"../data/NRTI_DataSet.txt",
              r"../data/NNRTI_DataSet.txt"]
 
     '''
@@ -58,11 +58,11 @@ def main():
     models = [
         "finetune_step-3300.ckpt",
         "bal_prior_step-1700.ckpt",
-        "full_model_step-2900.ckpt",
-        "label_enc_step-1000.ckpt",
-        "label_enc_step-2300.ckpt"
+        "full_model_step-2900.ckpt"
     ]
 
+    #models = ["label_enc_step-1000.ckpt",
+    #    "label_enc_step-2300.ckpt"]
     #models = ["tabpfn_full_step-1000.ckpt", "tabpfn_full_step-1600.ckpt",]
 
     feature_prefix = "F"
@@ -98,7 +98,7 @@ def main():
             multi_target_pfn = TabICLClassifier(model_path="../my/" + model, allow_auto_download=False, average_logits=True,
                                                 n_jobs=2, verbose=True, use_hierarchical=False)
 
-            #multi_target_pfn = TabPFNClassifier(model_path="../my/" + model, n_jobs=2, random_state=42)
+            #multi_target_pfn = TabPFNClassifier(model_path="../my/" + model, n_preprocessing_jobs=2, random_state=42)
 
 
             if use_kfold == False:
