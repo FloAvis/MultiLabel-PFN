d00d7d29f047cb73a101de6339a590dcbdaa2a87
FloAvis <152602773+FloAvis@users.noreply.github.com>
2025-11-04 14:04:38 +0100

    predictiong tabpfn attemtp 11

--- MultiLabelTabPFN/src/tabpfn/classifier.py
+++ MultiLabelTabPFN/src/tabpfn/classifier.py
@@ -126,7 +126,7 @@ class TabPFNClassifier(ClassifierMixin, BaseEstimator):
     """The number of labels found in the target data during `fit()`."""
 
     class_counts_: npt.NDArray[Any]
-    """The number of classes per label found in the target data during `fit()`."""
+    """The number of classes per class found in the target data during `fit()`."""
 
     n_outputs_: Literal[1]
     """The number of outputs the model has. Only 1 for now"""
@@ -526,11 +526,11 @@ class TabPFNClassifier(ClassifierMixin, BaseEstimator):
             self.classes_ = torch.arange(self.n_classes_)
         '''
         # TODO: Support more classes with a fallback strategy.
-        if self.n_labels_ > self.interface_config_.MAX_NUMBER_OF_LABELS:
+        if self.n_classes_ > self.interface_config_.MAX_NUMBER_OF_CLASSES:
             raise ValueError(
-                f"Number of labels {self.n_labels_} exceeds the maximal number of "
-                "labels supported by TabPFN. Consider using a strategy to reduce "
-                "the number of labels. For code see "
+                f"Number of classes {self.n_classes_} exceeds the maximal number of "
+                "classes supported by TabPFN. Consider using a strategy to reduce "
+                "the number of classes. For code see "
                 "https://github.com/PriorLabs/tabpfn-extensions/blob/main/src/"
                 "tabpfn_extensions/many_class/many_class_classifier.py",
             )
